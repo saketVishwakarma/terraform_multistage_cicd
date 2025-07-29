e24e7aeeb411e70c26901e9c72362607e0a997c3
SAKET KUMAR VISHWAKARMA <svsaket123@gmail.com>
2025-07-30 02:14:52 +0530

    Update main.tf

--- modules/virtualMachine/main.tf
+++ modules/virtualMachine/main.tf
@@ -12,7 +12,7 @@ resource "azurerm_network_interface" "nic" {
     ip_configuration {
        name                          = "internal"
        subnet_id                     = var.subnet_id
-       private_ip_address_allocation = "Static"
+       private_ip_address_allocation = "Dynamic"
        public_ip_address_id          = azurerm_public_ip.public_ip.id
      }
 }
