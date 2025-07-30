dad94ae0de75714dd7758ccb12442cbb9da3fcdc
SAKET KUMAR VISHWAKARMA <svsaket123@gmail.com>
2025-07-31 02:07:55 +0530

    Update output.tf

--- modules/database/output.tf
+++ modules/database/output.tf
@@ -5,3 +5,7 @@ output "private_dns_zone_id" {
   value = azurerm_private_dns_zone.postgres_dns.id
   
 }
+output "postgres_subnet_id" {
+  value = azurerm_subnet.postgres_subnet.id
+  description = "ID of the subnet delegated to PostgreSQL Flexible Server"
+}
