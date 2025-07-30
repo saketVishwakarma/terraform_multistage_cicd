580bebee2528a005a04f12c7ea81d151050eb19a
SAKET KUMAR VISHWAKARMA <svsaket123@gmail.com>
2025-07-31 02:15:15 +0530

    Update main.tf

--- modules/database/main.tf
+++ modules/database/main.tf
@@ -2,21 +2,7 @@ resource "azurerm_private_dns_zone" "postgres_dns" {
   name                = "privatelink.postgres.database.azure.com"
   resource_group_name = var.resource_group_name
 }
-resource "azurerm_subnet" "postgres_subnet" {
-  name                 = "postgres-subnet"
-  resource_group_name  = var.resource_group_name
-  virtual_network_id = var.vnet_name
-  address_prefixes     = ["10.0.3.0/24"]
 
-  delegation {
-    name = "delegation"
-
-    service_delegation {
-      name    = "Microsoft.DBforPostgreSQL/flexibleServers"
-      actions = ["Microsoft.Network/virtualNetworks/subnets/action"]
-    }
-  }
-}
 
 resource "azurerm_private_dns_zone_virtual_network_link" "vnet_link" {
   name                  = "devvnet-link"
