3b1bee395be3b5942049e11a8638a04b0ac693bb
SAKET KUMAR VISHWAKARMA <svsaket123@gmail.com>
2025-07-31 00:22:23 +0530

    Update main.tf

--- modules/network/main.tf
+++ modules/network/main.tf
@@ -10,6 +10,18 @@ resource "azurerm_subnet" "subnet" {
     address_prefixes     = [var.subnets[count.index].address_prefix]
     resource_group_name  = var.resource_group_name
     virtual_network_name = azurerm_virtual_network.vnet.name
+    dynamic "delegation" {
+    for_each = var.subnets[count.index].delegate_postgres ? [1] : []
+    content {
+      name = "delegation"
+      service_delegation {
+        name = "Microsoft.DBforPostgreSQL/flexibleServers"
+        actions = [
+          "Microsoft.Network/virtualNetworks/subnets/action"
+        ]
+      }
+    }
+}
     lifecycle {
         create_before_destroy = true
     }
