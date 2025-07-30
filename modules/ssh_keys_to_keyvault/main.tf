c01a983806f40bb61687392473c6f4a46463f413
SAKET KUMAR VISHWAKARMA <svsaket123@gmail.com>
2025-07-30 11:49:33 +0530

    Update main.tf

--- modules/ssh_keys_to_keyvault/main.tf
+++ modules/ssh_keys_to_keyvault/main.tf
@@ -3,16 +3,3 @@ resource "tls_private_key" "ssh_key" {
   rsa_bits  = 4096
 }
 
-# Store Private Key in Key Vault
-resource "azurerm_key_vault_secret" "private_key" {
-  name         = var.private_key_secret_name
-  value        = tls_private_key.ssh_key.private_key_pem
-  key_vault_id = var.keyvault_id
-}
-
-# Store Public Key in Key Vault
-resource "azurerm_key_vault_secret" "public_key" {
-  name         = var.public_key_secret_name
-  value        = tls_private_key.ssh_key.public_key_openssh
-  key_vault_id = var.keyvault_id
-}
