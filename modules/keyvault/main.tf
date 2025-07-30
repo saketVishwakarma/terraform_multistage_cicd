715dad419c6d5ba284b07211d5d0f590b2dc54f5
saketVishwakarma <svsaket123@gmail.com>
2025-07-30 15:05:25 +0530

    updated image sku and offer

--- modules/keyvault/main.tf
+++ modules/keyvault/main.tf
@@ -11,8 +11,8 @@ resource "azurerm_key_vault" "vault" {
   access_policy {
     tenant_id = var.tenant_id
     object_id = var.object_id
-    key_permissions = ["Get", "List"]
-    secret_permissions = ["Get", "Set"]
+    key_permissions = ["Get", "List","Delete","Update"]
+    secret_permissions = ["Get", "Set","Delete","Update"]
   }
   lifecycle {
     prevent_destroy = true
